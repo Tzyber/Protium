f1d7814ed02e75979bd94f468bb3eac7033f50f2
Dominik Dörrier <134324777+Tzyber@users.noreply.github.com>
2026-07-25 03:07:41 +0200

    fixed flashbang on program start

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./ui/App.vue";
@@ -5,3 +6,9 @@ import "./ui/fonts.css";
 import "./ui/tokens.css";
 
 createApp(App).use(createPinia()).mount("#app");
+
+requestAnimationFrame(() => {
+    void getCurrentWindow()
+    .show()
+    .catch(() => {});
+});
